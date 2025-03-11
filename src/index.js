c55cdaf628fdc65b983810f376c59add92f7a4ce
Krista Koivisto <krista.koivisto@paf.com>
2025-03-11 11:23:22 +0200

    chore: add link to public APIs project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Together from 'together-ai';
 // https://docs.together.ai/docs/introduction
 //
 // (Switch from Python to TypeScript to get more useful example code.)
+//
+// Need ideas? How about some public APIs?
+// https://github.com/public-apis/public-apis
 
 const together = new Together({
   apiKey: process.env.TOGETHER_API_KEY,
