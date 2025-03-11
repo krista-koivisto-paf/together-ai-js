2c7086de8ebb56d9d9a06dde18d7cfa717ff8772
Krista Koivisto <krista.koivisto@paf.com>
2025-03-11 11:10:40 +0200

    chore: add comment about documentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Together from 'together-ai';
 
+// Documentation:
+// https://docs.together.ai/docs/introduction
+//
+// (Switch from Python to TypeScript to get more useful example code.)
+
 const together = new Together({
   apiKey: process.env.TOGETHER_API_KEY,
 });
